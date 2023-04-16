f17f779d7fe3aa49ac6198e0ca798e187f7b565c
haleeIT2000 <halee752000@gmail.com>
2023-04-16 21:49:15 +0700

    fix file

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
         <Route path="/addteaching" element={<AddTeaching/>}/>
         <Route path="/add-term-exam" element={<AddTermExam/>}/>
         <Route path="/add-research" element={<AddResearch/>}/>
-        <Route path="/add-scientific-articles" element={<AddScientificArticles/>}/>
+        <Route path="/add-article" element={<AddScientificArticles/>}/>
         <Route path="/add-thesis" element={<AddThesis/>}/>
         <Route path="/add-book" element={<AddTextBook/>}/>
         <Route path="/add-instructions" element={<AddInstruction/>}/>
